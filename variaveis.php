3a5b5781be69b07495d356bec8a76850b09b4ed4
Thiago Bertolino <40404294+thiagobertolino@users.noreply.github.com>
2021-03-30 10:02:05 -0300

    Add files via upload

---
+++ variaveis.php
@@ -0,0 +1,36 @@
+<?php
+// Meus dados
+
+$nome = "Thiago Bertolino";
+$idade = "21 anos";
+$altura = "1.75m";
+
+echo "Meu nome é $nome, minha idade é $idade, minha altura é $altura <br>";
+
+// Alterando meus dados
+
+$nome = "Bertolino";
+$idade = 23;
+$altura = 1.9;
+
+echo "Meu nome é $nome, minha idade é $idade, minha altura é $altura <br>";
+echo "<hr>";
+
+$carro = "Fiat Punto";
+$meunome = "TH";
+
+echo $carro;
+echo "<br>";
+echo $meunome;
+
+// Nomes válidos
+//$nome
+//$_meunome
+//$carro10
+//$nota1
+//$meu_nome
+
+// Nomes inválidos
+//$123456nome
+//$carro@!#
+//$meu carro
